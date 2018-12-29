05015c976603dbb2a07a18e1bb8705633d99f567
Nayan Kumar <nayan@answerwise.io>
2018-12-29 10:01:26 +0530

    create product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Product(models.Model):
+	title = models.CharField(max_length=255)
+	url = models.TextField()
+	pub_date = models.DateTimeField(default=datetime.now)
+	votes_total = models.IntegerField(default=1)
+	image = models.ImageField(upload_to='images/')
+	icon = models.ImageField(upload_to='images/')
+	body = models.TextField()
+	hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def summary(self):
+		return self.body[:100]
+
+	def __str__(self):
+		return self.title
